44a4d3bbe1fe1cb23aa1ac8ca2a85fd69ed1c2b6
samantaavijit <samantaavijit.2000@gmail.com>
2021-03-15 19:02:08 +0530

    Initial commit

---
+++ algorithms.py
@@ -0,0 +1,21 @@
+import visualizer
+
+
+def bubbleSort(array, name):
+    for i in range(len(array)):
+        for j in range(len(array) - i - 1):
+            if array[j] > array[j + 1]:
+                array[j], array[j + 1] = array[j + 1], array[j]
+            visualizer.updateDisplay(array=array, algorithmName=name, swap1=array[j], swap2=array[j + 1])
+    visualizer.updateDisplay(array=array, algorithmName=name, status="Done!")
+
+
+def selectionSort(array, name):
+    for i in range(len(array)):
+        min_index = i
+        for j in range(i + 1, len(array)):
+            if array[j] < array[min_index]:
+                min_index = j
+            visualizer.updateDisplay(array=array, algorithmName=name, swap1=array[min_index], swap2=array[j])
+        array[i], array[min_index] = array[min_index], array[i]
+    visualizer.updateDisplay(array=array, algorithmName=name, status="Done!")
---
+++ visualizer.py
@@ -0,0 +1,59 @@
+import pygame
+import sys
+import random
+
+pygame.init()
+WIDTH = 600
+HEIGHT = 512
+bar_width = 10
+space = 2
+delay = 100
+
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+screen.fill((255, 255, 255))
+
+
+def check_events():
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+
+def updateDisplay(array, algorithmName, swap1=None, swap2=None, status="Running", sc=screen):
+    pygame.time.delay(delay)
+    screen.fill((255, 255, 255))
+    if status == "Running":
+        pygame.display.set_caption("{} Algorithm Visualizer   Status {}....".format(algorithmName, status))
+    else:
+        pygame.display.set_caption("{} Algorithm Visualizer   Status {}".format(algorithmName, status))
+
+    for i in range(len(array)):
+        x = (i * bar_width) + (i * space)
+        # + (WIDTH - (len(array) * bar_width + len(array) * space)) / 2
+        colour = (80, 0, 255)
+        if swap1 == array[i]:
+            colour = (0, 255, 0)
+        elif swap2 == array[i]:
+            colour = (255, 0, 0)
+        pygame.draw.rect(sc, colour, (x, WIDTH, bar_width, -array[i]))
+    check_events()
+    pygame.display.update()
+
+
+def generate_arr():
+    a = random.sample(range(100, HEIGHT), 50)
+    return a
+
+
+if __name__ == '__main__':
+    import algorithms
+
+    array = generate_arr()
+    # for i in array:
+    #     print(i, end=" ")
+    updateDisplay(array, "Bubble Sort")
+    # algorithms.bubbleSort(array,"Bubble Sort")
+    algorithms.selectionSort(array, "Selection Sort")
+    while True:
+        check_events()
